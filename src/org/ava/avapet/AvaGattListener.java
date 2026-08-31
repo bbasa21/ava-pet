c14cefd7c49b9e03883d2694daba550baed2e1b5
bbasa21 <alipourgholi236@gmail.com>
2026-08-31 21:01:59 +0330

    Remove obsolete custom GATT listener API

--- src/org/ava/avapet/AvaGattListener.java
+++ src/org/ava/avapet/AvaGattListener.java
@@ -1,13 +1,8 @@
 package org.ava.avapet;
 
-import android.bluetooth.BluetoothGatt;
-import android.bluetooth.BluetoothGattCharacteristic;
-import android.bluetooth.BluetoothGattDescriptor;
-
+/**
+ * Deprecated compatibility stub. The app no longer uses this interface.
+ * GATT callbacks are handled entirely by AvaGattCallback.java.
+ */
 public interface AvaGattListener {
-    void onConnectionStateChange(BluetoothGatt gatt, int status, int newState);
-    void onServicesDiscovered(BluetoothGatt gatt, int status);
-    void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic);
-    void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status);
-    void onDescriptorWrite(BluetoothGatt gatt, BluetoothGattDescriptor descriptor, int status);
 }
